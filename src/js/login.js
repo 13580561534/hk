b5fbb9d51f1b1ecffb3b2a2d9a3ce3537816281a
wn <1249746820@qq.com>
2019-05-21 10:52:23 +0800

    第一次提交

---
+++ src/js/login.js
@@ -0,0 +1,50 @@
+/* 
+ * @Author: Marte
+ * @Date:   2019-05-18 17:02:59
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2019-05-21 08:33:07
+ */
+
+$(function() {
+
+
+            $('#loginsubmit').on('click', function() {
+                    // debugger
+                    //用户名
+                    var uname = $('#loginname').val();
+                    var psw = $('#nloginpwd').val();
+
+                    if (uname == "" || psw == "") {
+                        $('.msg-error').css('display','block');
+                        // alert("用户名或者密码不能为空")
+                    }
+                    else{
+                        if (getCookie('uname')){
+                                alert('先退出账号');
+                            }else{
+                                $.ajax({
+                                    type: 'post',
+                                    url: '../api/login.php',
+                                    data: {
+                                        name:uname,
+                                        psw:psw
+                                    },
+                                    success: function(str) {
+                                        console.log(str);
+                                        if (str == '1') {
+                                            // $.cookie('uname', $('.itxt').val(),7);
+                                            setCookie('username',uname,1);
+                                            location.href ='../sy.html?'+uname;
+                                            //把这个用户名存到cookie里面
+                                            console.log(uname);
+                                             
+                                        } else {
+                                            alert('登录失败');
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    })
+
+            });
